da09619ba387b4505f019ee120ac8f057961e775
Sara <sara@thoughtstem.com>
2020-02-03 15:12:34 -0800

    updating deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,17 @@
 #lang info
 (define collection "mc-coach-training")
-(define deps '("base"))
+(define deps '("base"
+               "https://github.com/thoughtstem/website.git"
+               "https://github.com/thoughtstem/mc-coach.git"
+               "https://github.com/thoughtstem/define-assets-from.git"
+               "reprovide-lang-lib"
+               "https://github.com/thoughtstem/metacoders-dot-org.git?path=metacoders-dot-org-lib"
+               "https://github.com/thoughtstem/mc-classmaps.git"
+               "https://github.com/thoughtstem/stories.git"
+               "https://github.com/thoughtstem/impress.git"
+               "https://github.com/thoughtstem/TS-GE-Katas.git?path=ts-kata-util"
+               "https://github.com/thoughtstem/TS-GE-Katas.git?path=ts-survival"
+               ))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/mc-coach-training.scrbl" ())))
 (define pkg-desc "Description Here")
