d9d28634d3157ddcb1e47827a5761325514b9190
Sara <sara@thoughtstem.com>
2020-02-14 11:47:35 -0800

    Update intro.rkt

--- books/book-1/chapter-9/stories/intro.rkt
+++ books/book-1/chapter-9/stories/intro.rkt
@@ -9,15 +9,19 @@
 (define links   (list))
 
 @paras{
-  Welcome back to Metapolis!  This time, we find ourselves in the middle of June: all schools have let out for the summer and the long days are sunny and warm.  All citizens of Metapolis are spending as many hours in the beautiful outdoors as they are able!
+  Welcome back to Metapolis!
 
-  In this final chapter of Book 1, we will be learning about the glue that holds a MetaCoders class together; what connects independant Story Modes and Game Modes into one coherent class.  The theme of this chapter is @b{classroom transitions} -- the final key to running a smooth and successful class.
+  In this final chapter of Book 1, we will be learning about the glue that holds a MetaCoders class together, what connects independent Story Modes and Game Modes into one coherent class.  The theme of this chapter is @b{classroom transitions} -- the final key to running a smooth and successful class.
 
-  Ruby, an experienced coach, will meet you at @b{Coder Cafe} to talk about the tips and tricks of successful transitions before you head to @b{Kick Butt Martial Arts} to see her put those techniques into practice in a Friday evening class.
+  Here's your itinerary:
+
+  @(ol
+    (li "Ruby, an experienced coach, will meet you at " (b "Hopper Library") " to give you some tips and tricks of successful transitions.")
+    (li "Then, you'll head to " (b "Kick Butt Martial Arts") " to see Ruby but those techniques into practice."))
 
   Believe it or not, you're more than ready to begin coaching your first MetaCoders course.
 
-  You have a github acount.  You've made multiple commits.  You've created your coach story. __, __, .... You've learned about Story Mode and narrative transportation; Game Mode and flow state; .... 
+  You have a github acount.  You've made multiple commits.  You've created your coach story.  You've learned about Story Mode and narrative transportation; Game Mode and flow state; Classmaps and, after this chapter, how to knit those modes into a cohesive class. 
   
   There's always more to learn,  but that's for Book 2.  Take some time to reflect on what you've learned.  Feel good about yourself.  We're proud of you and excited to see what you will do!
 }
